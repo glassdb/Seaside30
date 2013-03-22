db02a277c76dfeceb0af7593189b920520d3d006
Dale Henrichs <dhenrich@vmware.com>
2013-03-21 20:05:11 -0700

    gearing up for using Travis for running Seaside30 tests

---
+++ tests/testTravisCI.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -x
+#
+#  GLASS test driver for running GLASS builds on travisCI ... for now the code
+#    is hosted elsewhere, but ultimately the packages will be managed here.
+#
+#  This boyo generates an old-style configuration load script similar to those 
+#    used by GemTools and builderCI in before_gemstone.sh, but we're testing
+#    a barebones load and test so no need for Metacello Scripting API, to be
+#    pre-installed.
+#
+#      -verbose flag causes unconditional transcript display
+#
+# Copyright (c) 2013 VMware, Inc. All Rights Reserved <dhenrich@vmware.com>.
+#
+
+OUTPUT_PATH="${PROJECT_HOME}/tests/travisCI.st"
+
+cat - >> $OUTPUT_PATH << EOF
+| gitPath |
+Transcript cr; show: 'travis--->${OUTPUT_PATH}'.
+Metacello image
+  project: 'GLASS';
+  get.
+Metacello
+  project: 'GLASS';
+  version: '${GLASS}';
+  load.
+gitPath := (FileDirectory default directoryNamed: 'git_cache') fullName.
+
+"Load Seaside30 from git repository"
+
+Metacello new
+  baseline: 'Seaside30';
+  repository: 'filetree://', gitPath, '/filetree/repository';
+  get.
+
+Metacello new
+  baseline: 'Seaside30';
+  repository: 'filetree://', gitPath, '/filetree/repository';
+  load: '#( ${LOADS} )'.
+
+TravisCIHarness
+  value: #( 'BaselineOfSeaside30' )
+  value: 'TravisCISuccess.txt' 
+  value: 'TravisCIFailure.txt'.
+
+EOF
+
+cat $OUTPUT_PATH
+
+$BUILDER_CI_HOME/testTravisCI.sh "$@"
+ 
